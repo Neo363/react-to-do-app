c2003b008a05588aa75ca7061de9083284393333
Niraj Gurung <nirajguroong363@gmail.com>
2021-12-07 12:38:26 +0545

    first react-todo-app commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import TodoItemsRemaining from './TodoItemsRemaining';
+import TodoClearCompleted from './TodoClearCompleted';
+import TodoFilters from './TodoFilters';
+
+TodoList.propTypes = {
+  todos: PropTypes.array.isRequired,
+  todosFiltered: PropTypes.func.isRequired,
+  completeTodo: PropTypes.func.isRequired,
+  markAsEditing: PropTypes.func.isRequired,
+  updateTodo: PropTypes.func.isRequired,
+  cancelEdit: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+  remaining: PropTypes.func.isRequired,
+  clearCompleted: PropTypes.func.isRequired
+}
+
+function TodoList(props) {
+  const [filter, setFilter] = useState('all');
+
+  return (
+    <>
+      <ul className="todo-list">
+        {props.todosFiltered(filter).map((todo, index) => (
+          <li key={todo.id} className="todo-item-container">
+            <div className="todo-item">
+              <input
+                type="checkbox"
+                onChange={() => props.completeTodo(todo.id)}
+                checked={todo.isComplete ? true : false}
+              />
+
+              {!todo.isEditing ? (
+                <span
+                  onDoubleClick={() => props.markAsEditing(todo.id)}
+                  className={`todo-item-label ${
+                    todo.isComplete ? "line-through" : ""
+                  }`}
+                >
+                  {todo.title}
+                </span>
+              ) : (
+                <input
+                  type="text"
+                  onBlur={(event) => props.updateTodo(event, todo.id)}
+                  onKeyDown={(event) => {
+                    if (event.key === "Enter") {
+                      props.updateTodo(event, todo.id);
+                    } else if (event.key === "Escape") {
+                      props.cancelEdit(todo.id);
+                    }
+                  }}
+                  className="todo-item-input"
+                  defaultValue={todo.title}
+                  autoFocus
+                />
+              )}
+            </div>
+
+            <button className="x-button" onClick={() => props.deleteTodo(todo.id)}>
+              <svg
+                className="x-button-icon"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <div className="check-all-container">
+        <div>
+          <div className="button">Check All</div>
+        </div>
+
+        <TodoItemsRemaining remaining={props.remaining}/>
+      </div>
+
+      <div className="other-buttons-container">
+        <TodoFilters 
+          todosFiltered={props.todosFiltered}
+          filter={filter}
+          setFilter={setFilter}
+        />
+        <div>
+          <TodoClearCompleted clearCompleted={props.clearCompleted}/>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TodoList;
